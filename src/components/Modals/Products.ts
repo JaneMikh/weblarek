79430f2a17b21b348a1771986032a96ee94c703c
Evgeniya Mikhaylyuk <jmatricaria@yandex.ru>
2025-10-10 17:44:21 +0300

    Частично заполнена документация в readme, написаны интерфейсы и класс каталога товаров

---
+++ src/components/Modals/Products.ts
@@ -0,0 +1,36 @@
+import { IProduct } from "../../types";
+
+export class Products {
+  private items: IProduct[];
+  private selectedItem!: IProduct | null;
+
+  constructor(items: IProduct[] = []) {
+    this.items = items;
+    this.selectedItem = null;
+  }
+
+  // Получение массива товаров из модели
+  getItemsList(): IProduct[] {
+    return this.items;
+  }
+
+  //Сохранение массива товаров, полученного в параметрах метода
+  setItemsList(items: IProduct[]) : void {
+    this.items = items;
+  }
+
+  //Получение карточки товара по id
+  getItemById(id: string): IProduct | undefined {
+    return this.items.find(item => item.id === id);
+  }
+
+  //Сохранение выбранной карточки товара для подробного отображения
+  saveSelectedItem(item: IProduct) : void {
+    this.selectedItem = item;
+  }
+
+  //Получение информации о товаре для его подробного отображения в отдельном окне
+  getSelectedItem() : IProduct | null {
+    return this.selectedItem;
+  }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,27 @@
 export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
 
+// Способ оплаты
+export type TPayment = 'card' | 'cash' | '';
+
 export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+}
+
+//Товар
+export interface IProduct {
+  id: string;   // Уникальный идентификатор товара
+  description: string; // Описание товара
+  image: string; // Ссылка на изображение товара
+  title: string;  // Название товара
+  category: string; // Категория товара
+  price: number | null; // Цена товара
+}
+
+//Покупатель
+export interface IBuyer {
+  payment: TPayment;  // Способ оплаты
+  email: string; // Адрес электронной почты полкупателя
+  phone: string; // Номер телефона покупателя
+  address: string; // Адрес доставки
 }
